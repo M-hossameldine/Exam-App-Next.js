f9780fd3a6c1346342dec1733c03a691fd15e2b3
M-hossameldine <m.hossameldine@gmail.com>
2025-12-08 23:20:24 +0200

    chore: add middleware matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -31,3 +31,16 @@ export default async function middleware(request: NextRequest) {
     new URL(DEFAULT_PROTECTED_ROUTE, request.nextUrl.origin)
   );
 }
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
