f9346284a4b4f981f8ecda65c04fc60beaedaf43
M-hossameldine <m.hossameldine@gmail.com>
2025-12-14 23:42:04 +0200

    chore: add sidebar provider and useMobile hook

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -2,6 +2,7 @@
 
 import { NextAuthProvider } from './_components/next-auth.provider';
 import ReactQueryProvider from './_components/react-query-provider';
+import AppSidebarProvider from './_components/app-sidebar-provider';
 import { SessionProvider } from 'next-auth/react';
 
 type ProviderProps = {
@@ -12,7 +13,9 @@ export function Providers({ children }: ProviderProps) {
   return (
     <NextAuthProvider>
       <ReactQueryProvider>
-        <SessionProvider>{children}</SessionProvider>
+        <SessionProvider>
+          <AppSidebarProvider>{children}</AppSidebarProvider>
+        </SessionProvider>
       </ReactQueryProvider>
     </NextAuthProvider>
   );
