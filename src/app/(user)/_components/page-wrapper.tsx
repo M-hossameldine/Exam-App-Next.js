53f73d8d7ddb76795d264d5e993ee659b4d1632d
M-hossameldine <m.hossameldine@gmail.com>
2025-12-14 21:28:46 +0200

    feature: allow vertical scroll for authorized page-wrapper

--- src/app/(user)/_components/page-wrapper.tsx
+++ src/app/(user)/_components/page-wrapper.tsx
@@ -11,7 +11,7 @@ export default function PageWrapper({
   children,
 }: PageWrapperProps) {
   return (
-    <main className="h-screen w-full flex flex-col items-stretch bg-secondary-50">
+    <main className="h-screen w-full flex flex-col items-stretch bg-secondary-50 overflow-y-auto">
       <PageHeader title={title} icon={icon} goBackUrl={goBackUrl} />
 
       <div className="px-6 pb-6 w-full">{children}</div>
